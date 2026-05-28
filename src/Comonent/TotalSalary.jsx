676cbff70829c0f41a8113351e11c1588fbcdced
Sanjay Ghadigaonkar <sanjay07.scg@gmail.com>
2026-05-28 12:24:28 +0530

    Expence Tracker

--- src/Comonent/TotalSalary.jsx
+++ src/Comonent/TotalSalary.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const TotalSalary = ({ amount }) => {
   return (
     // Parent div: Box ko center mein rakhne ke liye
-    <div className="flex justify-center w-full px-2 mt-4">
+    <div className="flex justify-center w-full px-2 mt-4 ">
       {/* 1. md:max-w-sm: Desktop/Laptop par width ko chhota (limit) karega.
         2. w-full: Mobile par poori width lega.
         3. py-10: Upar aur niche se padding dekar box ki height badhayega.
